refactored utilities and examples_EP.py, corrected gradient direction for noise_variance - test next. Need to refactor other examples around this commit.

examples/example7/exampleTemp.py
@@ -1,5 +1,5 @@
 """
-Multiclass oredered probit regression 3 bin example from Cowles 1996 empirical study
+Ordered probit regression 3 bin example from Cowles 1996 empirical study
 showing convergence of the orginal probit with the Gibbs sampler.
 """
 import argparse
@@ -8,114 +8,18 @@ from io import StringIO
 from pstats import Stats, SortKey
 import numpy as np
 from scipy.stats import multivariate_normal
-from probit.samplers import GibbsMultinomialOrderedGPTemp
+from probit.samplers import GibbsOrderedGPTemp
 from probit.kernels import SEIso
 import matplotlib.pyplot as plt
 import pathlib
+from probit.data.utilities import generate_prior_data, generate_synthetic_data, get_Y_trues, colors, datasets, metadata
+
 
 write_path = pathlib.Path()
 
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+# Septiel varphi=30.0, scale=20.0
+# Tertile varphi=30.0, noise_variance=0.1
 
-
-def generate_synthetic_data(N_per_class, K, kernel):
-    """
-    Generate synthetic data for this model.
-
-    :arg int N_per_class: The number of data points per class.
-    :arg int K: The number of bins/classes/quantiles.
-    :arg int D: The number of dimensions of the covariates.
-    """
-    N_total = int(K * N_per_class)
-
-    # Sample from the real line, uniformly
-    #X = np.random.uniform(0, 12, N_total)
-    X = np.linspace(0., 1., N_total)  # 500 points evenly spaced over [0,1]
-    X = X[:, None]  # reshape X to make it n*D
-    mu = np.zeros((N_total))  # vector of the means
-
-    C = kernel.kernel_matrix(X, X)
-
-    print(np.shape(mu))
-    print(np.shape(C))
-    print("1")
-    cutpoint_0 = np.inf
-    while np.abs(cutpoint_0) > 5.0:
-        print(cutpoint_0)
-        Z = np.random.multivariate_normal(mu, C)
-        plt.figure()  # open new plotting window
-        plt.plot(X[:], Z[:])
-        plt.show()
-        epsilons = np.random.normal(0, 1, N_total)
-        # Model latent variable responses
-        Y_true = epsilons + Z
-        sort_indeces = np.argsort(Y_true)
-        plt.scatter(X, Y_true)
-        plt.show()
-        # Sort the responses
-        Y_true = Y_true[sort_indeces]
-        X = X[sort_indeces]
-        X_k = []
-        Y_true_k = []
-        t_k = []
-        for k in range(K):
-            X_k.append(X[N_per_class * k:N_per_class * (k + 1)])
-            Y_true_k.append(Y_true[N_per_class * k:N_per_class * (k + 1)])
-            t_k.append(k * np.ones(N_per_class, dtype=int))
-        # Find the first cutpoint and set it equal to 0.0
-        cutpoint_0_min = Y_true_k[0][-1]
-        cutpoint_0_max = Y_true_k[1][0]
-        print(cutpoint_0_max, cutpoint_0_min)
-        cutpoint_0 = np.mean([cutpoint_0_max, cutpoint_0_min])
-    Y_true = np.subtract(Y_true, cutpoint_0)
-    Y_true_k = np.subtract(Y_true_k, cutpoint_0)
-    for k in range(K):
-        plt.scatter(X_k[k], Y_true_k[k], color=colors[k])
-    plt.show()
-    Xs_k = np.array(X_k)
-    Ys_k = np.array(Y_true_k)
-    t_k = np.array(t_k, dtype=int)
-    X = Xs_k.flatten()
-    Y = Ys_k.flatten()
-    t = t_k.flatten()
-    # Prepare data
-    Xt = np.c_[Y, X, t]
-    print(np.shape(Xt))
-    np.random.shuffle(Xt)
-    Y_true = Xt[:, :1]
-    X = Xt[:, 1:D + 1]
-    t = Xt[:, -1]
-    print(np.shape(X))
-    print(np.shape(t))
-    print(np.shape(Y_true))
-    t = np.array(t, dtype=int)
-    print(t)
-    colors_ = [colors[i] for i in t]
-    print(colors_)
-    plt.scatter(X, Y_true, color=colors_)
-    plt.show()
-    return X_k, Y_true_k, X, Y_true, t
-
-
-def split(list, K):
-    """Split a list into quantiles."""
-    divisor, remainder = divmod(len(list), K)
-    return np.array(list[i * divisor + min(i, remainder):(i+1) * divisor + min(i + 1, remainder)] for i in range(K))
-
-
-# This is the general kernel for a GP prior for the multi-class problem
-# varphi = 30.0
-# scale = 20.0
-
-varphi = 0.01
-scale = 3.0
-sigma = 10e-6
-tau = 10e-6
-
-kernel = SEIso(varphi, scale, sigma=sigma, tau=tau)
-
-#argument = "diabetes_quantile"
-argument = "stocks_quantile"
 
 if argument == "diabetes_quantile":
     K = 5
@@ -230,7 +134,7 @@ else:
 # print(Y_true_k[1][-1], Y_true_k[2][0], "cutpoint 2")
 
 # Initiate classifier
-gibbs_classifier = GibbsMultinomialOrderedGPTemp(K, X, t, kernel)
+gibbs_classifier = GibbsOrderedGPTemp(K, X, t, kernel)
 steps_burn = 100
 steps = 5000
 

examples/example7/example_VB.py
@@ -8,17 +8,15 @@ from io import StringIO
 from pstats import Stats, SortKey
 import numpy as np
 from scipy.stats import multivariate_normal
-from probit.estimators import VBMultinomialOrderedGPSS, VBMultinomialOrderedGP
+from probit.estimators import VBOrderedGPSS, VBOrderedGP
 from probit.kernels import SEIso
 import matplotlib.pyplot as plt
 import pathlib
 import importlib.resources as pkg_resources
-from probit.utilities import generate_prior_data, generate_synthetic_data
-from . import data
+from probit.data.utilities import generate_prior_data, generate_synthetic_data, get_Y_trues, colors, datasets, metadata
 
 
 write_path = pathlib.Path()
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
 
 def split(list, K):
@@ -266,7 +264,7 @@ def ordinal_VB_training(X_train, t_train, X_test, t_test, gamma_0, K, varphi_0=1
     print("theta_0", theta)
     kernel = SEIso(varphi, scale, sigma=sigma, tau=tau)
     # Initiate classifier
-    variational_classifier = VBMultinomialOrderedGP(X_train, t_train, kernel)
+    variational_classifier = VBOrderedGP(X_train, t_train, kernel)
     res = minimize(variational_classifier.hyperparameter_training_step, theta, method='L-BFGS-B', jac=True, options={
         'maxiter':25})
     theta = res.x
@@ -322,8 +320,8 @@ def outer_loops():
             print(x_new)
             kernel = SEIso(x_new[0], x_new[1], sigma=sigma, tau=tau)
             # Initiate classifier
-            variational_classifier = VBMultinomialOrderedGP(X_train, t_train, kernel)
-            #variational_classifier = VBMultinomialOrderedGP(X, t, kernel)
+            variational_classifier = VBOrderedGP(X_train, t_train, kernel)
+            #variational_classifier = VBOrderedGP(X, t, kernel)
             steps = 50
             y_0 = Y_true.flatten()
             m_0 = y_0
@@ -501,8 +499,8 @@ def test_plots(X_test, X_train, t_test, t_train, Y_true):
     tau = 10e-6
     kernel = SEIso(varphi, scale, sigma=sigma, tau=tau)
     # Initiate classifier
-    variational_classifier = VBMultinomialOrderedGP(X_train, t_train, kernel)
-    #variational_classifier = VBMultinomialOrderedGP(X, t, kernel)
+    variational_classifier = VBOrderedGP(X_train, t_train, kernel)
+    #variational_classifier = VBOrderedGP(X, t, kernel)
     steps = 50
     y_0 = Y_true.flatten()
     m_0 = y_0

probit/estimators.py
@@ -1325,7 +1325,7 @@ class VBMultinomialGP(Estimator):
             return bound
 
 
-class VBMultinomialOrderedGPSS(Estimator):
+class VBOrderedGPSS(Estimator):
     """
     TODO: Superceded since it is not required that gamma_1 = 0.
 
@@ -1338,9 +1338,9 @@ class VBMultinomialOrderedGPSS(Estimator):
     """
     def __init__(self, *args, **kwargs):
         """
-        Create an :class:`VBMultinomialOrderedGP` Estimator object.
+        Create an :class:`VBOrderedGP` Estimator object.
 
-        :returns: An :class:`VBMultinomialOrderedGP` object.
+        :returns: An :class:`VBOrderedGP` object.
         """
         super().__init__(*args, **kwargs)
         self.IN = np.eye(self.N)
@@ -1689,7 +1689,7 @@ class VBMultinomialOrderedGPSS(Estimator):
         return bound
 
 
-class VBMultinomialOrderedGP(Estimator):
+class VBOrderedGP(Estimator):
     """
     A Variational Bayes classifier for ordered likelihood. Inherits the Estimator ABC
 
@@ -1700,7 +1700,7 @@ class VBMultinomialOrderedGP(Estimator):
     """
     def __init__(self, *args, **kwargs):
         """
-        Create an :class:`VBMultinomialOrderedGP` Estimator object.
+        Create an :class:`VBOrderedGP` Estimator object.
 
         :returns: An :class:`VBMultinimoalOrderedGP` object.
         """
@@ -1871,6 +1871,7 @@ class VBMultinomialOrderedGP(Estimator):
         """
         Return the w values of the sample
 
+        TODO: Is this the correct reference?
         Reference: M. Girolami and S. Rogers, "Variational Bayesian Multinomial Probit Regression with Gaussian Process
         Priors," in Neural Computation, vol. 18, no. 8, pp. 1790-1817, Aug. 2006, doi: 10.1162/neco.2006.18.8.1790.2005
         Page 9 Eq.(7).
@@ -2100,7 +2101,7 @@ class VBMultinomialOrderedGP(Estimator):
         return fx, gx
 
 
-class EPMultinomialOrderedGP(Estimator):
+class EPOrderedGP(Estimator):
     """
     An Expectation Propagation classifier for ordered likelihood. Inherits the Estimator ABC.
 
@@ -2115,9 +2116,9 @@ class EPMultinomialOrderedGP(Estimator):
     """
     def __init__(self, *args, **kwargs):
         """
-        Create an :class:`EPMultinomialOrderedGP` Estimator object.
+        Create an :class:`EPOrderedGP` Estimator object.
 
-        :returns: An :class:`EPMultinomialOrderedGP` object.
+        :returns: An :class:`EPOrderedGP` object.
         """
         super().__init__(*args, **kwargs)
         self.IN = np.eye(self.N)
@@ -2868,7 +2869,7 @@ class EPMultinomialOrderedGP(Estimator):
             Phi_new = np.dstack((xx, yy))
             Phi_new = Phi_new.reshape((len(x1s) * len(x2s), 2))
             fxs = np.empty(len(Phi_new))
-            gxs = np.empty(len(Phi_new))
+            gxs = np.empty((len(Phi_new),2))
         else:
             raise ValueError("Couldn't infer what you wanted to plot from the input arguments (gamma_0={}, varphi_0={}, noise_variance_0={})".format(
                 gamma_0, varphi_0, noise_variance_0))
@@ -3158,7 +3159,7 @@ class EPMultinomialOrderedGP(Estimator):
         # For gx[0] -- ln\sigma
         gx[0] += np.sum(t5 - t4)
         # gx[0] *= -0.5 * noise_variance  # This is not what is written in the paper. It is a typo in Chu code
-        gx[0] *= -np.sqrt(noise_variance)
+        gx[0] *= np.sqrt(noise_variance)
         # For gx[1] -- \b_1
         gx[1] += np.sum(t3 - t2)
         # For gx[2] -- ln\Delta^r

probit/utilities.py
@@ -1,167 +1,8 @@
-"""Utility functions."""
+"""Utility functions for probit."""
 import numpy as np
 from scipy.stats import norm, expon
 import math
 import matplotlib.pyplot as plt
-
-
-# For plotting
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-
-
-def generate_prior_data(N_per_class, K, D, kernel, noise_variance):
-    """
-    Generate data from the GP prior, and choose some cutpoints that approximately divides data into equal bins.
-
-    :arg int N_per_class: The number of data points per class.
-    :arg int K: The number of bins/classes/quantiles.
-    :arg int D: The number of data dimensions.
-    :arg kernel: The GP prior.
-    """
-    N_total = int(K * N_per_class)
-    # Sample from the real line, uniformly
-    # X = np.random.uniform(0, 12, N_total)
-    X = np.linspace(0., 1., N_total)  # 500 points evenly spaced over [0,1]
-    X = X[:, None]  # reshape X to make it n*D
-    mu = np.zeros((N_total))  # vector of the means
-    C = kernel.kernel_matrix(X, X)
-    Z = np.random.multivariate_normal(mu, C)
-    plt.figure()  # open new plotting window
-    plt.plot(X[:], Z[:])
-    plt.show()
-    epsilons = np.random.normal(0, np.sqrt(noise_variance), N_total)
-    # Model latent variable responses
-    Y_true = epsilons + Z
-    sort_indeces = np.argsort(Y_true)
-    plt.scatter(X, Y_true)
-    plt.show()
-    # Sort the responses
-    Y_true = Y_true[sort_indeces]
-    X = X[sort_indeces]
-    X_k = []
-    Y_true_k = []
-    t_k = []
-    gamma = np.empty(K + 1)
-    for k in range(K):
-        X_k.append(X[N_per_class * k:N_per_class * (k + 1)])
-        Y_true_k.append(Y_true[N_per_class * k:N_per_class * (k + 1)])
-        t_k.append(k * np.ones(N_per_class, dtype=int))
-    for k in range(1, K):
-        # Find the first cutpoint and set it equal to 0.0
-        cutpoint_k_min = Y_true_k[k - 1][-1]
-        cutpoint_k_max = Y_true_k[k][0]
-        gamma[k] = np.average([cutpoint_k_max, cutpoint_k_min])
-    gamma[0] = -np.inf
-    gamma[-1] = np.inf
-    print("gamma={}".format(gamma))
-    for k in range(K):
-        plt.scatter(X_k[k], Y_true_k[k], color=colors[k])
-    plt.show()
-    Xs_k = np.array(X_k)
-    Ys_k = np.array(Y_true_k)
-    t_k = np.array(t_k, dtype=int)
-    X = Xs_k.flatten()
-    Y = Ys_k.flatten()
-    t = t_k.flatten()
-    # Prepare data
-    Xt = np.c_[Y, X, t]
-    print(np.shape(Xt))
-    np.random.shuffle(Xt)
-    Y_true = Xt[:, :1]
-    X = Xt[:, 1:D + 1]
-    t = Xt[:, -1]
-    print(np.shape(X))
-    print(np.shape(t))
-    print(np.shape(Y_true))
-    t = np.array(t, dtype=int)
-    print(t)
-    colors_ = [colors[i] for i in t]
-    print(colors_)
-    plt.scatter(X, Y_true, color=colors_)
-    plt.show()
-    return X_k, Y_true_k, X, Y_true, t, gamma
-
-
-def generate_synthetic_data(N_per_class, K, D, kernel, noise_variance):
-    """
-    Generate synthetic data for this model.
-
-    This function will generate data such that the ground truth of the first cutpoint is at zero.
-
-    :arg int N_per_class: The number of data points per class.
-    :arg int K: The number of bins/classes/quantiles.
-    :arg int D: The number of data dimensions.
-    :arg kernel: The GP prior.
-    """
-    N_total = int(K * N_per_class)
-
-    # Sample from the real line, uniformly
-    #X = np.random.uniform(0, 12, N_total)
-    X = np.linspace(0., 1., N_total)  # 500 points evenly spaced over [0,1]
-    X = X[:, None]  # reshape X to make it n*D
-    mu = np.zeros((N_total))  # vector of the means
-
-    C = kernel.kernel_matrix(X, X)
-
-    print(np.shape(mu))
-    print(np.shape(C))
-    print("1")
-    cutpoint_0 = np.inf
-    while np.abs(cutpoint_0) > 5.0:
-        print(cutpoint_0)
-        Z = np.random.multivariate_normal(mu, C)
-        plt.figure()  # open new plotting window
-        plt.plot(X[:], Z[:])
-        plt.show()
-        epsilons = np.random.normal(0, np.sqrt(noise_variance), N_total)
-        # Model latent variable responses
-        Y_true = epsilons + Z
-        sort_indeces = np.argsort(Y_true)
-        plt.scatter(X, Y_true)
-        plt.show()
-        # Sort the responses
-        Y_true = Y_true[sort_indeces]
-        X = X[sort_indeces]
-        X_k = []
-        Y_true_k = []
-        t_k = []
-        for k in range(K):
-            X_k.append(X[N_per_class * k:N_per_class * (k + 1)])
-            Y_true_k.append(Y_true[N_per_class * k:N_per_class * (k + 1)])
-            t_k.append(k * np.ones(N_per_class, dtype=int))
-        # Find the first cutpoint and set it equal to 0.0
-        cutpoint_0_min = Y_true_k[0][-1]
-        cutpoint_0_max = Y_true_k[1][0]
-        print(cutpoint_0_max, cutpoint_0_min)
-        cutpoint_0 = np.mean([cutpoint_0_max, cutpoint_0_min])
-    Y_true = np.subtract(Y_true, cutpoint_0)
-    Y_true_k = np.subtract(Y_true_k, cutpoint_0)
-    for k in range(K):
-        plt.scatter(X_k[k], Y_true_k[k], color=colors[k])
-    plt.show()
-    Xs_k = np.array(X_k)
-    Ys_k = np.array(Y_true_k)
-    t_k = np.array(t_k, dtype=int)
-    X = Xs_k.flatten()
-    Y = Ys_k.flatten()
-    t = t_k.flatten()
-    # Prepare data
-    Xt = np.c_[Y, X, t]
-    print(np.shape(Xt))
-    np.random.shuffle(Xt)
-    Y_true = Xt[:, :1]
-    X = Xt[:, 1:D + 1]
-    t = Xt[:, -1]
-    print(np.shape(X))
-    print(np.shape(t))
-    print(np.shape(Y_true))
-    t = np.array(t, dtype=int)
-    print(t)
-    colors_ = [colors[i] for i in t]
-    print(colors_)
-    plt.scatter(X, Y_true, color=colors_)
-    plt.show()
-    return X_k, Y_true_k, X, Y_true, t
 
 
 def return_prob(b, t, K, gamma, noise_std, numerically_stable=True):
@@ -179,6 +20,7 @@ def return_prob(b, t, K, gamma, noise_std, numerically_stable=True):
         phi1 = norm.cdf((gamma[t] - b) / noise_std)
         phi2 = norm.cdf((gamma[t + 1] - b) / noise_std)
     return phi1 - phi2
+
 
 def fromb_fft1(b, mean, sigma, t, K, gamma, noise_variance, EPS):
     """
@@ -809,4 +651,3 @@ def matrix_of_valuess(nu, K, N_test):
     # Tile along the rows, as they are the elements of interest
     Lambdas_Ts = np.tile(nu, (1, 1, K))
     return Lambdas_Ts  # (N_test, K, K)
-