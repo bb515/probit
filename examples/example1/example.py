fixed gradients for Laplace. Gradients involving Rhomberg integration and VB need to be unit tested

examples/example1/example.py
@@ -132,9 +132,9 @@ def main():
         (X, f_, g_true, y,
         cutpoints_0, theta_0, noise_variance_0, signal_variance_0,
         J, D, colors, Kernel) = load_data_paper(dataset, plot=True)
-        from probit.kernels import SquaredExponentialARD
-        Kernel = SquaredExponentialARD
-        theta_0 = np.array([theta_0, theta_0])
+        # from probit.kernels import SquaredExponentialARD
+        # Kernel = SquaredExponentialARD
+        # theta_0 = np.array([theta_0, theta_0])
     else:
         raise ValueError("Dataset {} not found.".format(dataset))
     N_train = np.shape(y)[0]
@@ -155,49 +155,52 @@ def main():
     if kernel._ARD:
         trainables[-1] = [1, 1]
         # Fix theta
-        # trainables[-1] = [0] * int(D)
+        trainables[-1] = [0] * int(D)
     else:
         trainables[-1] = 1
         # Fix theta
         trainables[-1] = 0
-    # Fix noise variance
+    # Fix noise standard deviation
     trainables[0] = 0
-    # Fix signal variance
+    # Fix signal standard deviation
     trainables[J] = 0
     # Fix cutpoints
     trainables[1:J] = [0] * (J - 1)
+    trainables[1] = 1
     # trainables[J-1] = 1
     # trainables[J-2] = 1
-
+    print(trainables)
+    print(cutpoints_0)
+    print(signal_variance_0)
+    print(theta_0)
     # just theta
-    # domain = ((-1, 1.3), None)
-    # res = (20, None)
+    domain = ((-1, 1.3), None)
+    res = (30, None)
     # theta_0 and theta_1
-    domain = ((-1, 1.3), (-1, 1.3))
-    res = (20, 20)
-    # #  just signal variance, domain is log_10(signal_std)
+    # domain = ((-1, 1.3), (-1, 1.3))
+    # res = (20, 20)
+    # #  just signal standard deviation, domain is log_10(signal_std)
     # domain = ((0., 1.8), None)
     # res = (20, None)
-    # just noise variance, domain is log_10(noise_std)
+    # just noise std, domain is log_10(noise_std)
     # domain = ((-1., 1.0), None)
     # res = (100, None)
-    # # theta and signal variance
+    # # theta and signal std dev
     # domain = ((0, 2), (0, 2))
     # res = (100, None)
     # # cutpoints b_1 and b_2 - b_1
     # domain = ((-0.75, -0.5), (-1.0, 1.0))
     # res = (14, 14)
 
-    # grid_synthetic(classifier, domain, res, steps, trainables, show=False)
+    grid_synthetic(classifier, domain, res, steps, trainables, show=False)
 
     # plot(classifier, domain=None)
 
-    # classifier = train(classifier, method, trainables)
     # classifier = train(
     #     classifier, method, trainables, verbose=True, steps=steps)
     # test(classifier, X, y, g_true, steps)
 
-    grid_synthetic(classifier, domain, res, steps, trainables, show=True)
+    # grid_synthetic(classifier, domain, res, steps, trainables, show=True)
 
     # plot_synthetic(classifier, dataset, X_true, g_true, steps, colors=colors)
 

probit/approximators.py
@@ -320,9 +320,12 @@ class Approximator(ABC):
         """
         if cutpoints is not None:
             self.cutpoints = check_cutpoints(cutpoints, self.J)
+            print("cutpoints", self.cutpoints)
             self.cutpoints_ts = self.cutpoints[self.y_train]
             self.cutpoints_tplus1s = self.cutpoints[self.y_train + 1]
         if theta is not None or variance is not None:
+            print("theta", theta)
+            print("signal", np.sqrt(variance))
             self.kernel.update_hyperparameter(
                 theta=theta, variance=variance)
             # Update prior covariance
@@ -981,9 +984,15 @@ class VBGP(Approximator):
                 gx[2:self.J] *= intervals
                 if verbose:
                     print(gx[2:self.J])
-            # For gx[self.J] -- s
+            # For gx[J] -- s
             if trainables[self.J]:
-                raise ValueError("TODO")
+                partial_K_variance = self.kernel.kernel_partial_derivative_variance(
+                    self.X_train, self.X_train)
+                # VC * VC * a' * partial_K_theta * a / 2
+                gx[self.J] = self.kernel.variance * 0.5 * weight.T @ partial_K_variance @ weight  # That's wrong. not the same calculation.
+                # equivalent to -= theta * 0.5 * np.trace(cov @ partial_K_theta)
+                gx[self.J] -= self.kernel.variance * 0.5 * np.sum(np.multiply(partial_K_variance, cov))
+                gx[self.J] *= 2.0  # since theta = kappa / 2
             # For kernel parameters
             if self.kernel._ARD:
                 for d in range(self.D):
@@ -1010,6 +1019,7 @@ class VBGP(Approximator):
                         # Update gx[-1], the partial derivative of the lower bound
                         # wrt the lengthscale. Using matrix inversion Lemma
                         one = (theta / 2) * weight.T @ partial_K_theta @ weight
+                        # TODO: Seems to be missing a term that EP and LA have got!
                         # TODO: slower but what about @jit compile CPU or GPU?
                         # D = solve_triangular(
                         #     L_cov.T, partial_K_theta, lower=True)
@@ -1133,9 +1143,9 @@ class EPGP(Approximator):
         :returns: An :class:`EPGP` object.
         """
         super().__init__(*args, **kwargs)
-        self.EPS = 1e-4
+        # self.EPS = 1e-3
         # self.EPS = 1e-4  # perhaps too large
-        # self.EPS = 1e-6  # Decreasing EPS will lead to more accurate solutions but a longer convergence time.
+        self.EPS = 1e-6  # Decreasing EPS will lead to more accurate solutions but a longer convergence time.
         self.EPS_2 = self.EPS**2
         self.jitter = 1e-10
         # Initiate hyperparameters
@@ -1779,6 +1789,8 @@ class EPGP(Approximator):
                 self.cutpoints_tplus1s,
                 noise_variance, noise_std, self.EPS, self.EPS_2, self.N)
         t5[self.indices_where_0] = 0.0
+        # print("t4", t4)
+        # print("t5", t5)
         return t1, t2, t3, t4, t5
 
     def objective(
@@ -1862,13 +1874,17 @@ class EPGP(Approximator):
         if trainables is not None:
             # Update gx
             if trainables[0]:
+                # TODO: Doesn't work due to numerical instability, also need to check for algebraic error
                 # For gx[0] -- ln\sigma
-                gx[0] = np.sum(t5 - t4)
+                # gx[0] -= 1 / np.sqrt(noise_variance) * np.sum(np.multiply(cov, self.K))
                 # gx[0] *= -0.5 * noise_variance  # This is a typo in the Chu code
-                gx[0] *= np.sqrt(noise_variance)
+                gx[0] = np.sum(t5 - t4)
+                #gx[0] *= np.sqrt(noise_variance) / 2.0
             # For gx[1] -- \b_1
             if trainables[1]:
                 gx[1] = np.sum(t3 - t2)
+                gx[1] *= self.cutpoints[1]
+                print("here", gx[1])
             # For gx[2] -- ln\Delta^r
             for j in range(2, self.J):
                 if trainables[j]:
@@ -1881,8 +1897,6 @@ class EPGP(Approximator):
             # For gx[self.J] -- variance
             if trainables[self.J]:
                 # For gx[self.J] -- s
-                # TODO: Need to check this is correct: is it directly analogous to
-                # gradient wrt log theta?
                 partial_K_variance = self.kernel.kernel_partial_derivative_variance(
                     self.X_train, self.X_train)
                 # VC * VC * a' * partial_K_theta * a / 2
@@ -1892,7 +1906,6 @@ class EPGP(Approximator):
                 # ad-hoc Regularisation term - penalise large theta, but Occam's term should do this already
                 # gx[self.J] -= 0.1 * theta
                 gx[self.J] *= 2.0  # since theta = kappa / 2
-                print(gx[self.J])
             # For gx[self.J + 1] -- theta
             if self.kernel._ARD:
                 if trainables[self.J + 1]:
@@ -2024,7 +2037,7 @@ class PEPGP(Approximator):
         :returns: An :class:`EPGP` object.
         """
         super().__init__(*args, **kwargs)
-        self.EPS = 1e-4  # perhaps too large
+        # self.EPS = 1e-4  # perhaps too large
         # self.EPS = 1e-6  # Decreasing EPS will lead to more accurate solutions but a longer convergence time.
         self.EPS_2 = self.EPS**2
         self.jitter = 1e-6  # 1e-10  # was 1e-10
@@ -2702,15 +2715,12 @@ class PEPGP(Approximator):
         # random permutation of data
         indices = np.arange(self.N)
         while error / (steps * self.N) > self.EPS**2:
-        # for _ in range(10):
             iteration += 1
             (error, beta, gamma, mean_EP, variance_EP,
                 log_lik, grads) = self.run_pep_parallel(
             indices, steps, beta=beta, gamma=gamma,
             mean_EP=mean_EP, variance_EP=variance_EP, write=False)
             print("{}/iterations, error={}".format(iteration, error))
-        # TODO: Why not just directly calculate gradients given states
-        # TODO: A: because it requires a whole loop
         # Compute posterior TODO: does it need to be done twice?
         (posterior_mean, posterior_cov) = self._compute_posterior(
             self.Kuu, gamma, beta)
@@ -3106,8 +3116,8 @@ class LaplaceGP(Approximator):
             if trainables[1]:
                 # For gx[1], \phi_b^1
                 t2 = dsigma @ precision
-                t2 = t2 / precision
-                gx[1] -= np.sum(w2 - w1)
+                # t2 = t2 / precision
+                gx[1] = np.sum(w1 - w2)
                 gx[1] += 0.5 * np.sum(t1 * (1 - t2) * diag)
                 gx[1] = gx[1] / self.noise_std
             # For gx[2] -- ln\Delta^r
@@ -3138,7 +3148,15 @@ class LaplaceGP(Approximator):
                     gx[j] = gx[j] * intervals[j - 2] / self.noise_std
             # For gx[J] -- variance
             if trainables[self.J]:
-                raise ValueError("TODO")
+                partial_K_variance = self.kernel.kernel_partial_derivative_variance(
+                    self.X_train, self.X_train)
+                dmat = partial_K_variance @ cov
+                t2 = (dmat @ weight) / precision
+                # VC * VC * a' * partial_K_theta * a / 2
+                gx[self.J] = -self.kernel.variance * 0.5 * weight.T @ partial_K_variance @ weight  # That's wrong. not the same calculation.
+                # equivalent to -= theta * 0.5 * np.trace(cov @ partial_K_theta)
+                gx[self.J] += self.kernel.variance * 0.5 * np.trace(dmat)
+                gx[self.J] *= 2.0  # since theta = kappa / 2
             # For gx[J + 1] -- theta
             if self.kernel._ARD:
                 partial_K_theta = self.kernel.kernel_partial_derivative_theta(
@@ -3150,8 +3168,6 @@ class LaplaceGP(Approximator):
                         gx[self.J + 1 + d] -= self.kernel.theta[d] * 0.5 * weight.T @ partial_K_theta[d] @ weight
                         gx[self.J + 1 + d] += self.kernel.theta[d] * 0.5 * np.sum((-diag * t1 * t2) / (self.noise_std))
                         gx[self.J + 1 + d] += self.kernel.theta[d] * 0.5 * np.sum(np.multiply(cov, partial_K_theta[d]))
-                        # ad-hoc Regularisation term - penalise large theta, but Occam's term should do this already. Or is it numerical stability
-                        # gx[J] -= 0.1 * theta
             else:
                 if trainables[self.J + 1]:
                     partial_K_theta = self.kernel.kernel_partial_derivative_theta(
@@ -3161,8 +3177,6 @@ class LaplaceGP(Approximator):
                     gx[self.J + 1] -= self.kernel.theta * 0.5 * weight.T @ partial_K_theta @ weight
                     gx[self.J + 1] += self.kernel.theta * 0.5 * np.sum((-diag * t1 * t2) / (self.noise_std))
                     gx[self.J + 1] += self.kernel.theta * 0.5 * np.sum(np.multiply(cov, partial_K_theta))
-                    # ad-hoc Regularisation term - penalise large theta, but Occam's term should do this already
-                    # gx[J] -= 0.1 * theta
         return gx
 
     def _approximate_initiate(

probit/plot.py
@@ -377,6 +377,8 @@ def train(classifier, method, trainables, verbose=True, steps=None, max_sec=5000
     minimize_stopper = MinimizeStopper(max_sec=max_sec)
     phi = classifier.get_phi(trainables)
     args = (trainables, steps)
+    print("phi", phi)
+
     res = minimize(
         classifier.approximate_posterior, phi,
         args, method=method, jac=True,
@@ -891,7 +893,6 @@ def _grid_over_hyperparameters_initiate(
         theta_0.append(np.exp(phi_0[index]))
         index += 1
     if trainables[1]:
-        phi_0 = np.exp(phi_0)
         # Grid over b_1, the first cutpoint
         label.append(r"$b_{}$".format(1))
         axis_scale.append("linear")
@@ -912,8 +913,8 @@ def _grid_over_hyperparameters_initiate(
             theta_0.append(np.exp(phi_0[index]))
             index += 1
     if trainables[classifier.J]:
-        # Grid over variance
-        label.append("$variance$")
+        # Grid over signal variance
+        label.append("$\sigma_{\theta}^{2}$")
         axis_scale.append("log")
         theta = np.logspace(domain[index][0], domain[index][1], res[index])
         space.append(theta)
@@ -974,7 +975,7 @@ def _grid_over_hyperparameters_initiate(
         label[0], label[1],
         axis_scale[0], axis_scale[1],
         meshgrid_theta[0], meshgrid_theta[1],
-        phis, fxs, gxs, np.array(theta_0))
+        phis, fxs, gxs, np.array(theta_0), phi_0)
 
 
 def grid_synthetic(
@@ -985,7 +986,7 @@ def grid_synthetic(
     xscale, yscale,
     xx, yy,
     phis, fxs,
-    gxs, theta_0) = _grid_over_hyperparameters_initiate(
+    gxs, theta_0, phi_0) = _grid_over_hyperparameters_initiate(
         classifier, res, domain, trainables)
     for i, phi in enumerate(phis):
         print(phi)
@@ -1015,11 +1016,16 @@ def grid_synthetic(
     print("xscale={}, yscale={}".format(xscale, yscale))
     if ylabel is None:
         #Normalization:
-        #First derivatives: need to calculate them in the log domain
-        log_x = np.log(x)
-        dlog_x = np.diff(log_x)
-        dZ_ = np.gradient(Z, log_x)
-        dZ = np.diff(Z) / dlog_x
+        #First derivatives: need to calculate them in the log domain if theta is in log domain
+        if xscale == "log":
+            logx = np.log(x)
+            dx = np.diff(log_x)
+            dZ_ = np.gradient(Z, logx)
+            #dZ = np.diff(Z) / dlogx
+        elif xscale == "linear":
+            dx = np.diff(x)
+            dZ_ = np.gradient(Z, x)
+            #dZ = np.diff(Z) / dx
 
         fig = plt.figure()
         fig.patch.set_facecolor('white')
@@ -1032,17 +1038,22 @@ def grid_synthetic(
         if show: plt.show()
         plt.close()
 
+        idx_hat = np.argmin(Z)
         fig = plt.figure()
         fig.patch.set_facecolor('white')
         fig.patch.set_alpha(0.0)
         ax = fig.add_subplot(111)
         ax.grid()
-        ax.plot(x, Z, 'b')
-        ax.vlines(theta_0, 0.9 * ax.get_ylim()[0], 0.9 * ax.get_ylim()[1], 'k',
+        ax.plot(x, Z, 'b',  label=r"$\mathcal{F}}$")
+        ylim = ax.get_ylim()
+        ax.vlines(x[idx_hat], 0.99 * ylim[0], 0.99 * ylim[1], 'r',
+            alpha=0.5, label=r"$\hat\theta={:.2f}$".format(x[idx_hat]))
+        ax.vlines(theta_0, 0.99 * ylim[0], 0.99 * ylim[1], 'k',
             alpha=0.5, label=r"'true' $\theta$")
         ax.set_xlabel(xlabel)
         ax.set_xscale(xscale)
         ax.set_ylabel(r"$\mathcal{F}$")
+        ax.legend()
         fig.savefig("bound.png",
             facecolor=fig.get_facecolor(), edgecolor='none')
         if show: plt.show()
@@ -1078,14 +1089,14 @@ def grid_synthetic(
         ax = fig.add_subplot(111)
         ax.grid()
         ax.plot(
-            log_x, dZ_, 'r--',
+            x, dZ_, 'r--',
             label=r"$\frac{\partial \mathcal{F}}{\partial \theta}$ numeric")
         ax.vlines(
-            np.log(theta_0), 0.9 * ax.get_ylim()[0], 0.9 * ax.get_ylim()[1], 'k', alpha=0.5,
+            phi_0, 0.9 * ax.get_ylim()[0], 0.9 * ax.get_ylim()[1], 'k', alpha=0.5,
             label=r"'true' $\log \theta$")
         ax.set_ylim(ax.get_ylim())
         ax.plot(
-            log_x, grad, 'r',
+            x, grad, 'r',
             label=r"$\frac{\partial \mathcal{F}}{\partial \theta}$ analytic")
         ax.set_xlabel("log " + xlabel)
         ax.set_ylabel(
@@ -1099,21 +1110,21 @@ def grid_synthetic(
         fig = plt.figure()
         fig.patch.set_facecolor('white')
         fig.patch.set_alpha(0.0)
-        ax.grid()
         ax = fig.add_subplot(111)
-        ax.plot(log_x, Z, 'b', label=r"$\mathcal{F}}$")
-        ax.vlines(
-            np.log(theta_0), 0.9 * ax.get_ylim()[0], 0.9 * ax.get_ylim()[1], 'k', alpha=0.5,
-            label=r"'true' $\log \theta$")
+        ax.plot(x, Z, 'b', label=r"$\mathcal{F}}$")
         ax.plot(
-            log_x, dZ_, 'r--',
+            x, dZ_, 'r--',
             label=r"$\frac{\partial \mathcal{F}}{\partial \theta}$ numeric")
         ax.set_ylim(ax.get_ylim())
+        ax.vlines(
+            phi_0, 0.9 * ax.get_ylim()[0], 0.9 * ax.get_ylim()[1], 'k', alpha=0.5,
+            label=r"'true' $\log \theta$")
         ax.plot(
-            log_x, grad, 'r',
+            x, grad, 'r',
             label=r"$\frac{\partial \mathcal{F}}{\partial \theta}$ analytic")
         ax.set_xlabel("log " + xlabel)
         ax.legend()
+        ax.grid()
         plt.tight_layout()
         fig.savefig(
             "bound_grad.png", facecolor=fig.get_facecolor(), edgecolor='none')