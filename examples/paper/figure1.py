45a14aa0e8a24b5713a685e8e4ba2aa891b6e977
bb515 <bb515@cam.ac.uk>
2022-07-28 17:17:55 +0000

    removed a few numba dependencies

--- examples/paper/figure1.py
+++ examples/paper/figure1.py
@@ -15,8 +15,6 @@ os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
 os.environ["VECLIB_MAXIMUM_THREADS"] = "6" # export VECLIB_MAXIMUM_THREADS=4
 os.environ["NUMEXPR_NUM_THREADS"] = "6" # export NUMEXPR_NUM_THREADS=6
 os.environ["NUMBA_NUM_THREADS"] = "6"
-from numba import set_num_threads
-set_num_threads(6)
 
 import argparse
 import cProfile
