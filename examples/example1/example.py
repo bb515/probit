Fixed VB gradients and tidied lab and jax code. Ljax.jit compiled lab code seems as fast as jax code so probably depreciate jax code

examples/example1/example.py
@@ -1,9 +1,9 @@
 """Ordinal regression concrete examples. Approximate inference."""
 # Make sure to limit CPU usage
 import os
-# # Enable double precision
-# from jax.config import config
-# config.update("jax_enable_x64", True)
+#Enable double precision
+from jax.config import config
+config.update("jax_enable_x64", True)
 
 os.environ["OMP_NUM_THREADS"] = "6" # export OMP_NUM_THREADS=4
 os.environ["OPENBLAS_NUM_THREADS"] = "6" # export OPENBLAS_NUM_THREADS=4 
@@ -42,7 +42,7 @@ def get_approximator(
     elif approximation == "VB":
         from probit.approximators import VBGP
         # steps is the number of fix point iterations until check convergence
-        steps = np.max([10, N_train//10])
+        steps = np.max([10, N_train//1000])
         Approximator = VBGP
     elif approximation == "LA":
         from probit.approximators import LaplaceGP
@@ -136,9 +136,6 @@ def main():
         cutpoints_0, theta_0, noise_variance_0, signal_variance_0,
         J, D, colors, Kernel) = load_data_paper(
             dataset, J=J, D=D, ARD=False, plot=True)
-        # from probit.kernels import SquaredExponentialARD
-        # Kernel = SquaredExponentialARD
-        # theta_0 = np.array([theta_0, theta_0])
     else:
         raise ValueError("Dataset {} not found.".format(dataset))
     N_train = np.shape(y)[0]
@@ -153,7 +150,7 @@ def main():
         # Initiate classifier
         classifier = Approximator(
             cutpoints=cutpoints_0, noise_variance=noise_variance_0,
-            kernel=kernel, J=J, data=(X, y))
+            kernel=kernel, J=J, data=(X, y), single_precision=False)
 
     trainables = [1] * (J + 2)
     if kernel._ARD:
@@ -163,14 +160,14 @@ def main():
     else:
         trainables[-1] = 1
         # Fix theta
-        trainables[-1] = 1
+        trainables[-1] = 0
     # Fix noise standard deviation
-    trainables[0] = 0
+    trainables[0] = 1
     # Fix signal standard deviation
     trainables[J] = 0
     # Fix cutpoints
     trainables[1:J] = [0] * (J - 1)
-    # trainables[2] = 1
+    trainables[1] = 0
     print("trainables = {}".format(trainables))
     # just theta
     domain = ((-1, 1), None)

probit/jax/VB.py
@@ -1,7 +1,7 @@
-import lab as B
-from math import inf
-from scipy.stats import expon
-from probit.lab.utilities import (
+import jax.numpy as jnp
+from jax.scipy.stats import expon
+from jax.scipy.linalg import cholesky
+from probit.jax.utilities import (
     truncated_norm_normalising_constant, matrix_inverse, log_over_sqrt_2_pi)
 
 
@@ -32,10 +32,10 @@ def objective_VB(
     return (0.5 * trace_posterior_cov_div_var
         + 0.5 * trace_K_inv_posterior_cov
         + 0.5 * posterior_mean.T @ weight
-        - 0.5 * N * B.log(noise_variance)
+        - 0.5 * N * jnp.log(noise_variance)
         - 0.5 * log_det_cov
         - 0.5 * N
-        - B.sum(B.log(Z)))
+        - jnp.sum(jnp.log(Z)))
 
 
 def objective_gradient_VB(
@@ -90,11 +90,11 @@ def objective_gradient_VB(
     if trainables is not None:
         # For gx[0] -- ln\sigma
         if trainables[0]:
-            z1s = B.where(z1s == -inf, 0.0, z1s)
-            z1s = B.where(z1s == inf, 0.0, z1s)
-            z2s = B.where(z2s == -inf, 0.0, z2s)
-            z2s = B.where(z2s == inf, 0.0, z2s)
-            gx[0] = -trace_posterior_cov_div_var - B.sum(
+            z1s = jnp.where(z1s == -jnp.inf, 0.0, z1s)
+            z1s = jnp.where(z1s == jnp.inf, 0.0, z1s)
+            z2s = jnp.where(z2s == -jnp.inf, 0.0, z2s)
+            z2s = jnp.where(z2s == jnp.inf, 0.0, z2s)
+            gx[0] = -trace_posterior_cov_div_var - jnp.sum(
                 (z1s * norm_pdf_z1s - z2s * norm_pdf_z2s) / Z)
             if verbose: print("gx_sigma = ", gx[0])
         if trainables[1:J]:
@@ -103,18 +103,18 @@ def objective_gradient_VB(
         # For gx[1] -- \b_1
         if trainables[1]:
             # For gx[1], \phi_b^1
-            gx[1] = B.sum(w1 - w2)
+            gx[1] = jnp.sum(w1 - w2)
             gx[1] = gx[1] / noise_std
         # For gx[2] -- ln\Delta^r
         for j in range(2, J):
             # Prepare D f / D delta_l
             if trainables[j]:
-                gx[j] -= B.sum(B.where(y_train == j - 1, w2, 0))
-                gx[j] -= B.sum(B.where(y_train > j - 1, w2 - w1, 0))
+                gx[j] -= jnp.sum(jnp.where(y_train == j - 1, w2, 0))
+                gx[j] -= jnp.sum(jnp.where(y_train > j - 1, w2 - w1, 0))
                 gx[j] = gx[j] * intervals[j - 2] / noise_std
         if trainables[J]:
             gx[J] = (- variance * weight.T @ partial_K_variance @ weight
-                + variance * B.sum(B.multiply(partial_K_variance, cov)))
+                + variance * jnp.sum(jnp.multiply(partial_K_variance, cov)))
         # For kernel parameters
         if ARD:
             for d in range(D):
@@ -124,7 +124,7 @@ def objective_gradient_VB(
                         # lower bound wrt the lengthscale
                         gx[J + 1 + d] = (
                     - theta[d] / 2 * weight.T @ partial_K_theta[d] @ weight
-                    + theta[d] / 2 * B.einsum(
+                    + theta[d] / 2 * jnp.einsum(
                         'ij, ji ->', partial_K_theta[d], cov))
                         if verbose: print("gx = {}".format(gx[J + 1 + d]))
         else:
@@ -134,7 +134,7 @@ def objective_gradient_VB(
                     # wrt the lengthscale
                     gx[J + 1] = (
                         - theta / 2 * weight.T @ partial_K_theta @ weight
-                        + theta / 2 * B.einsum(
+                        + theta / 2 * jnp.einsum(
                             'ij, ji ->', partial_K_theta, cov))
                     if verbose: print("gx = {}".format(gx[J + 1]))
     return gx
@@ -155,10 +155,10 @@ def update_posterior_covariance_VB(noise_variance, N, K):
     """Update posterior covariances.
     # TODO: rename to Jacobian?
     """
-    cov, L_cov = matrix_inverse(noise_variance * B.eye(N) + K, N)
-    log_det_cov = -2 * B.sum(B.log(B.diag(L_cov)))
-    trace_cov = B.sum(B.diag(cov))
-    trace_posterior_cov_div_var = B.einsum(
+    cov, L_cov = matrix_inverse(noise_variance * jnp.eye(N) + K, N)
+    log_det_cov = -2 * jnp.sum(jnp.log(jnp.diag(L_cov)))
+    trace_cov = jnp.sum(jnp.diag(cov))
+    trace_posterior_cov_div_var = jnp.einsum(
         'ij, ij -> ', K, cov)
     return L_cov, cov, log_det_cov, trace_cov, trace_posterior_cov_div_var
 
@@ -187,9 +187,9 @@ def _theta_hyperparameters(
     :return: The approximate posterior mean of the hyperhyperparameters psi
         Girolami and Rogers Page 9 Eq.(10).
     """
-    return B.divide(
-        B.add(1, theta_hyperhyperparameters[0]),
-        B.add(theta_hyperhyperparameters[1], theta))
+    return jnp.divide(
+        jnp.add(1, theta_hyperhyperparameters[0]),
+        jnp.add(theta_hyperhyperparameters[1], theta))
 
 
 def _theta(
@@ -211,18 +211,18 @@ def _theta(
     """
     thetas = sample_thetas(  # (n_samples, N, N) for ISO case, depends on shape of theta hyper-hyperparameter
         theta_hyperparameters, n_samples)  # (n_samples, )
-    log_thetas = B.log(thetas)
+    log_thetas = jnp.log(thetas)
     Ks_samples = self.kernel.kernel_matrices(
         self.X_train, self.X_train, thetas)  # (n_samples, N, N)
-    Ks_samples = B.add(Ks_samples, self.epsilon * B.eye(N))
+    Ks_samples = jnp.add(Ks_samples, self.epsilon * jnp.eye(N))
     if vectorised:
         raise ValueError("TODO")
     else:
-        log_ws = B.empty((n_samples,))
+        log_ws = jnp.empty((n_samples,))
         # Scalar version
         for i in range(n_samples):
-            L_K = B.cholesky(Ks_samples[i])
-            half_log_det_K = B.sum(B.log(B.diag(L_K)))  # correct sign?
+            L_K = cholesky(Ks_samples[i])
+            half_log_det_K = jnp.sum(jnp.log(jnp.diag(L_K)))  # correct sign?
             # TODO 2021 - something not quite right - converges to zero
             # TODO: 28/07/2022 this may have been fixed by a fixing bug in the
             # code that did not update theta? Test
@@ -231,14 +231,14 @@ def _theta(
             log_ws[i] = log_over_sqrt_2_pi - half_log_det_K\
                 - 0.5 * posterior_mean.T @ weight
     # Normalise the w vectors
-    max_log_ws = B.max(log_ws)
-    log_normalising_constant = max_log_ws + B.log(
-        B.sum(B.exp(log_ws - max_log_ws), axis=0))
-    log_ws = B.subtract(log_ws, log_normalising_constant)
-    print(B.sum(B.exp(log_ws)))
-    element_prod = B.add(log_thetas, log_ws)
-    element_prod = B.exp(element_prod)
-    return B.sum(element_prod, axis=0)
+    max_log_ws = jnp.max(log_ws)
+    log_normalising_constant = max_log_ws + jnp.log(
+        jnp.sum(jnp.exp(log_ws - max_log_ws), axis=0))
+    log_ws = jnp.subtract(log_ws, log_normalising_constant)
+    print(jnp.sum(jnp.exp(log_ws)))
+    element_prod = jnp.add(log_thetas, log_ws)
+    element_prod = jnp.exp(element_prod)
+    return jnp.sum(element_prod, axis=0)
 
 
 def sample_thetas(theta_hyperparameter, n_samples):
@@ -258,7 +258,7 @@ def sample_thetas(theta_hyperparameter, n_samples):
     """
     # scale = theta_hyperparameter
     scale = 1. / theta_hyperparameter
-    shape = B.shape(theta_hyperparameter)
+    shape = jnp.shape(theta_hyperparameter)
     if shape == ():
         size = (n_samples,)
     else:

probit/lab/Laplace.py
@@ -1,21 +1,18 @@
 from cmath import inf
-import lab as B
+import lab.jax as B
 from math import inf
 from probit.lab.utilities import (
     truncated_norm_normalising_constant, matrix_inverse)
-# TODO: temp
-import numpy as np
 
 
 def update_posterior_LA(noise_std, noise_variance, posterior_mean,
         cutpoints_ts, cutpoints_tplus1s, K, N,
-        upper_bound=None, upper_bound2=None, tolerance=None):
+        upper_bound=None, upper_bound2=None):
     """Update Laplace approximation posterior covariance in Newton step."""
     (Z, norm_pdf_z1s, norm_pdf_z2s, z1s, z2s,
         _, _) = truncated_norm_normalising_constant(
             cutpoints_ts, cutpoints_tplus1s, noise_std, posterior_mean,
-            upper_bound=upper_bound, upper_bound2=upper_bound2,
-            tolerance=tolerance)
+            upper_bound=upper_bound, upper_bound2=upper_bound2)
     weight = (norm_pdf_z1s - norm_pdf_z2s) / Z / noise_std
     # This is not for numerical stability, it is mathematically correct
     z1s = B.where(z1s == -inf, 0.0, z1s)
@@ -38,15 +35,14 @@ def update_posterior_LA(noise_std, noise_variance, posterior_mean,
 def compute_weights_LA(
         posterior_mean, cutpoints_ts, cutpoints_tplus1s, noise_std,
         noise_variance,
-        N, K,
-        upper_bound=None, upper_bound2=None, tolerance=None):
+        N, K, upper_bound=None, upper_bound2=None):
+    # TODO: combine this with update_posterior_LA
     # Numerically stable calculation of ordinal likelihood!
     (Z,
     norm_pdf_z1s, norm_pdf_z2s,
     z1s, z2s, *_) = truncated_norm_normalising_constant(
         cutpoints_ts, cutpoints_tplus1s, noise_std,
-        posterior_mean, upper_bound=upper_bound, upper_bound2=upper_bound2,
-        tolerance=tolerance)
+        posterior_mean, upper_bound=upper_bound, upper_bound2=upper_bound2)
     w1 = norm_pdf_z1s / Z
     w2 = norm_pdf_z2s / Z
     # This is not for numerical stability, it is mathematically correct
@@ -69,23 +65,21 @@ def compute_weights_LA(
 
 
 def objective_LA(weight, posterior_mean, precision, L_cov, Z):
-    fx = -B.sum(B.log(Z))
-    fx += 0.5 * posterior_mean.T @ weight
-    fx += B.sum(B.log(B.diag(L_cov)))
-    fx += 0.5 * B.sum(B.log(precision))
-    return fx
+    return (-B.sum(B.log(Z))
+        + 0.5 * posterior_mean.T @ weight
+        + B.sum(B.log(B.diag(L_cov)))
+        + 0.5 * B.sum(B.log(precision)))
 
 
 def objective_gradient_LA(
         gx, intervals, w1, w2, g1, g2, v1, v2, q1, q2,
         cov, weight, precision, y_train, trainables, K, partial_K_theta,
         partial_K_variance, noise_std, noise_variance, theta, variance,
-        N, J, D, ARD):
+        J, D, ARD):
     if trainables is not None:
         # diagonal of posterior covariance
         dsigma = cov @ K
         diag = B.diag(dsigma) / precision
-        # partial lambda / partial phi_b = - partial lambda / partial f (* SIGMA)
         t1 = ((w2 - w1) - 3.0 * (w2 - w1) * (g2 - g1)
             - 2.0 * (w2 - w1)**3 - (v2 - v1)) / noise_variance
         # Update gx
@@ -102,8 +96,7 @@ def objective_gradient_LA(
                 - (g2 - g1)
                 + (g2 - g1)**2
                 + (q2 - q1)) / noise_variance)
-            gx[0] = B.sum(g2 - g1 + 0.5 * (tmp - t2 * t1) * diag)
-            gx[0] = - gx[0] / 2.0 * noise_variance
+            gx[0] = B.sum(g2 - g1) + B.sum((tmp - t2 * t1) * diag / 2.0)
         # For gx[1] -- \b_1
         if trainables[1]:
             # For gx[1], \phi_b^1
@@ -118,35 +111,26 @@ def objective_gradient_LA(
             cache0 = -(g2 + (w2 - w1) * w2) / noise_variance
             cache1 = - (g2 - g1 + (w2 - w1)**2) / noise_variance
             if trainables[j]:
-                idxj = np.where(y_train == j - 1)
-                idxg = np.where(y_train > j - 1)
-                idxl = np.where(y_train < j - 1)
-                cache = np.zeros(N)
-                cache[idxj] = cache0[idxj]
-                cache[idxg] = cache1[idxg]
-                t2 = dsigma @ cache
-                t2 = t2 / precision
-                gx[j] -= B.sum(w2[idxj])
-                temp = (
-                    w2[idxj]
-                    - 2.0 * (w2[idxj] - w1[idxj]) * g2[idxj]
-                    - 2.0 * (w2[idxj] - w1[idxj])**2 * w2[idxj]
-                    - v2[idxj]
-                    - (g2[idxj] - g1[idxj]) * w2[idxj]) / noise_variance
-                gx[j] += 0.5 * B.sum((temp - t2[idxj] * t1[idxj]) * diag[idxj])
-                gx[j] -= B.sum(w2[idxg] - w1[idxg])
-                gx[j] += 0.5 * B.sum(t1[idxg] * (1.0 - t2[idxg]) * diag[idxg])
-                gx[j] += 0.5 * B.sum(-t2[idxl] * t1[idxl] * diag[idxl])
+                cache = B.where(y_train == j - 1, cache0, 0)
+                cache = B.where(y_train > j - 1, cache1, cache)
+                t2 = dsigma @ cache / precision
+                gx[j] -= B.sum(B.where(y_train == j - 1, w2, 0))
+                gx[j] += 0.5 * B.sum(B.where(y_train == j - 1,
+                    ((w2 - 2.0 * (w2 - w1) * g2
+                    - 2.0 * (w2 - w1)**2 * w2
+                    - v2
+                    - (g2 - g1) * w2) / noise_variance - t2 * t1) * diag, 0))
+                gx[j] -= B.sum(B.where(y_train > j - 1, w2 - w1, 0))
+                gx[j] += 0.5 * B.sum(B.where(y_train > j - 1, t1 * (1.0 - t2) * diag, 0))
+                gx[j] += 0.5 * B.sum(B.where(y_train < j - 1, -t2 * t1 * diag, 0))
                 gx[j] = gx[j] * intervals[j - 2] / noise_std
         # For gx[J] -- variance
         if trainables[J]:
             dmat = partial_K_variance @ cov
             t2 = (dmat @ weight) / precision
-            # VC * VC * a' * partial_K_theta * a / 2
-            gx[J] = -variance * 0.5 * weight.T @ partial_K_variance @ weight  # That's wrong. not the same calculation.
-            # equivalent to -= theta * 0.5 * jnp.trace(cov @ partial_K_theta)
+            gx[J] = -variance * 0.5 * weight.T @ partial_K_variance @ weight
             gx[J] += variance * 0.5 * B.trace(dmat)
-            gx[J] *= 2.0  # since theta = kappa / 2
+            gx[J] *= 2.0
         # For gx[J + 1] -- theta
         if ARD:
             for d in range(D):

probit/sparse.py
@@ -9,7 +9,6 @@ import warnings
 import numpy as np
 from probit.utilities import read_array, read_scalar
 from probit.lab.utilities import truncated_norm_normalising_constant
-from probit.lab.VB import _p
 from scipy.linalg import cholesky, solve_triangular
 
 
@@ -130,6 +129,7 @@ class SparseVBGP(VBGP):
             self, steps, posterior_mean_0=None, first_step=1,
             write=False):
         """
+        # TODO: refactor into format like nonsparse
         Estimating the posterior means are a 3 step iteration over posterior_mean,
         theta and psi Eq.(8), (9), (10), respectively or,
         optionally, just an iteration over posterior_mean.
@@ -155,9 +155,12 @@ class SparseVBGP(VBGP):
         for _ in trange(first_step, first_step + steps,
                         desc="GP priors Sampler Progress", unit="samples",
                         disable=True):
-            p = _p(
-                posterior_mean, self.cutpoints_ts, self.cutpoints_tplus1s,
-                self.noise_std, self.EPS, self.upper_bound, self.upper_bound2)
+            (Z, norm_pdf_z1s, norm_pdf_z2s,
+                _, _) = truncated_norm_normalising_constant(
+                self.cutpoints_ts, self.cutpoints_tplus1s, self.noise_std,
+                posterior_mean, upper_bound=self.upper_bound,
+                upper_bound2=self.upper_bound2)
+            p =  (norm_pdf_z1s - norm_pdf_z2s) / Z
             g = self._g(
                 p, posterior_mean, self.noise_std)
             posterior_mean, weight = self._posterior_mean(