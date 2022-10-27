0250fbfe62a9c2f5feb16ef3280546d50ef91763
Toby Boyne <tobyboyne@gmail.com>
2022-10-27 15:13:22 +0000

    Add plotting scripts

--- probit_jax/plots/plot_catastrophic.py
+++ probit_jax/plots/plot_catastrophic.py
@@ -3,6 +3,8 @@ import jax.numpy as jnp
 import jax
 
 from probit_jax.implicit.utilities import norm_pdf, norm_cdf, _safe_Z, hessian_log_probit_likelihood
+from probit_jax.implicit.truncnorm import norm_cdf_dif
+
 
 def norm_cdf_jitter(z, jitter):
     return 0.5 * (1 + jax.lax.erf(z/jnp.sqrt(2))) * (1 - 2*jitter) + jitter
@@ -13,7 +15,7 @@ likelihood_parameters = (
     [-jnp.inf, 0, 10, jnp.inf]
 )
 
-f = jnp.linspace(0, 10)
+f = jnp.linspace(0, 5)
 y=0
 
 cutpoints_tplus1 = likelihood_parameters[1][y + 1]
@@ -28,19 +30,16 @@ for jitter in (0., 1e-3):
     Z = (norm_pdf(z2s) - norm_pdf(z1s)) / (norm_cdf_jitter(z2s, jitter) - norm_cdf_jitter(z1s, jitter))
     ax.plot(f, jnp.clip(Z, -1e10, 1e10), label=f"jitter={jitter}")
 
-Z, z1s, z2s = _safe_Z(f, y, likelihood_parameters)
-print(jnp.min((Z)))
-ax.twinx().plot(f, jnp.log(Z), label="Hmm")
+# Z, z1s, z2s = _safe_Z(f, y, likelihood_parameters)
+Z = norm_cdf_dif(z1s, z2s) + 1e-16
+# ax.twinx().plot(f, jnp.log(Z), label="Hmm")
 Z = (norm_pdf(z2s) - norm_pdf(z1s)) / Z
 ax.plot(f, Z, linestyle="--", label="Approximated Z")
+# ax.plot(f, Z, linestyle="--", label="Approximated Z")
 
-
+print(Z)
 ax.set_ylim((Z.min()-1, Z.max()+1))
 fig.legend()
 fig.savefig("probit_jax/plots/catastrophic.png")
-
-print(
-    hessian_log_probit_likelihood(10, y, likelihood_parameters)
-)
 
 # find linear approximation
--- probit_jax/plots/plot_compare_truncnorm.py
+++ probit_jax/plots/plot_compare_truncnorm.py
@@ -4,22 +4,27 @@ implementation"""
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
-from probit_jax.implicit.utilities import grad_log_probit_likelihood
+from probit_jax.implicit.utilities import grad_log_probit_likelihood, hessian_log_probit_likelihood
 from probit_jax.implicit.truncnorm import tnmean
 
-f = jnp.linspace(-8, 8, num=200)
+f = jnp.linspace(4, 5, num=200)
 fig, ax = plt.subplots()
 sigma = 0.1
 dZ1 = grad_log_probit_likelihood(f, 1, [sigma, [-jnp.inf, 0, 1, jnp.inf]])
+Zhessi = hessian_log_probit_likelihood(f, 1, [sigma, [-jnp.inf, 0, 1, jnp.inf]])
 # dZ2 = grad_log_probit_likelihood(f, 1, [sigma, [-jnp.inf, 0, 5, jnp.inf]])
 ax.plot(f, dZ1, label="gradlog")
+# ax.plot(f, Zhessi, label="hessian")
 # ax.plot(f, dZ2)
 # ax.plot(f, -f / sigma ** 2)
 
 z1 = -f / sigma
 z2 = (-f + 1) / sigma
 dZtn = tnmean(z1, z2) / sigma
-ax.plot(f, dZtn, label="tnmean", alpha=0.5)
+# ax.plot(f, dZtn, label="tnmean", alpha=0.5)
+
+#linear region
+ax.plot(f, -z2 / sigma, label="linear")
 
 # xs = jnp.array([-1, -0.5])
 # ys = grad_log_probit_likelihood(xs, 1, [sigma, [-jnp.inf, 0, 0.5, jnp.inf]])
