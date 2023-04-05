11feb38c66b20806d113118530f0ba7929fdeddf
TobyBoyne <tobyboyne@gmail.com>
2023-04-05 10:19:51 +0000

    Move rfm directory

--- probit/rfm.py
+++ probit/rfm.py
@@ -3,22 +3,121 @@ import jax.numpy as jnp
 from jax import random
 from jax import jacrev, jacfwd
 from jax import jit, vmap, grad
-from probit_api.model import LaplaceM
+# from probit_api.model import LaplaceM
 import lab.jax as B
-from probit_jax.implicit.solvers import (
+from probit.implicit.solvers import (
     fwd_solver, newton_solver,
     fixed_point_layer, fixed_point_layer_fwd, fixed_point_layer_bwd)
-from probit_jax.approximators import LaplaceGP
+from probit.approximators import LaplaceGP
+import matrix
+from algebra.util import identical
 
+from plum import dispatch
+from mlkernels import Kernel, pairwise, elwise
+
+class LaplaceM(Kernel):
+    """Weighted Laplace kernel.
+
+    Args:
+        M (array): Weight matrix of the weighted Laplace kernel.
+    """
+
+    def __init__(self, M):
+        self.M  = M
+
+    def _compute(self, dist2):
+        return B.exp(-B.sqrt(dist2))
+
+    def _pw_dists2(self, a, b):
+        a = B.uprank(a)
+        b = B.uprank(b)
+
+        # Optimise the one-dimensional case.
+        if B.shape(a, -1) == 1 and B.shape(b, -1) == 1:
+            return self.M * (a -B.transpose(b))**2
+
+        a_norm = (a @ self.M) * a
+        a_norm = B.sum(a_norm, axis=1, squeeze=False)
+
+        if a is b:
+            b_norm = a_norm
+        else:
+            b_norm = (b @ self.M) * b
+            b_norm = B.sum(b_norm, axis=1, squeeze=False)
+        b_norm = B.transpose(b_norm)
+        # b_norm = B.reshape(b_norm, (1, -1))  # alternative
+
+        distances = a @ (self.M @ B.transpose(b))
+        distances = distances * -2
+        distances = distances + a_norm
+        distances = distances + b_norm
+        distances = B.where(distances < 1e-10, 0.0, distances)
+        return distances
+
+    def _ew_dists2(self, a, b):
+        a = B.uprank(a)
+        b = B.uprank(b)
+
+        # Optimise the one-dimensional case.
+        if B.shape(a, -1) == 1 and B.shape(b, -1) == 1:
+            return self.M * (a -b)**2
+
+        a_norm = (a @ self.M) * a
+        a_norm = B.sum(a_norm, axis=1, squeeze=True)
+
+        if a is b:
+            b_norm = a_norm
+        else:
+            b_norm = (b @ self.M) * b
+            b_norm = B.sum(b_norm, axis=1, squeeze=True)
+
+        distances = (a @ self.M) * b
+        distances = B.sum(distances, axis=1, squeeze=True)
+        distances = distances * -2
+        distances = distances + a_norm
+        distances = distances + b_norm
+        distances = B.where(distances < 1e-10, 0.0, distances)
+        return distances
+
+    def render(self, formatter):
+        # This method determines how the kernel is displayed.
+        return "LaplaceM()"
+
+    @property
+    def _stationary(self):
+        # This method can be defined to return `True` to indicate that the kernel is
+        # stationary. By default, kernels are assumed to not be stationary.
+        return True
+
+    @dispatch
+    def __eq__(self, other: "LaplaceM"):
+        # If `other` is also a `EQWithLengthScale`, then this method checks whether
+        # `self` and `other` can be treated as identical for the purpose of
+        # algebraic simplifications. In this case, `self` and `other` are identical
+        # for the purpose of algebraic simplification if `self.scale` and
+        # `other.scale` are. We use `algebra.util.identical` to check this condition.
+        return identical(self.M, other.M)
+
+
+@pairwise.dispatch
+def pairwise(k: LaplaceM, x: B.Numeric, y: B.Numeric):
+    pw_dists2 = k._pw_dists2(x, y)
+    return matrix.Dense(k._compute(pw_dists2))
+
+
+@elwise.dispatch
+def elwise(k: LaplaceM, x: B.Numeric, y: B.Numeric):
+    ew_dists2 = k._ew_dists2(x, y)
+    return k._compute(ew_dists2)
 
 class RFM(LaplaceGP):
 
     def __init__(
             self, *args, **kwargs):
         """
-        Create an :class:`VBGP` Approximator object.
+        Create an :class:`RFM` Approximator object.
 
-        :returns: A :class:`VBGP` object.
+        :returns: A :class:`RFM` object.
         """
         super().__init__(*args, **kwargs)
         # Set up a JAX-transformable function for a custom VJP rule definition
